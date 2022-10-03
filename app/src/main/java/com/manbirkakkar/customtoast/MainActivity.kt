2b4532f21d70a01c65ea661305a3fafc8521cc4b
Manbir Kakkar <manbir.kakkar@shipsy.co.in>
2022-10-03 17:06:11 +0530

    Add sample for Toast lib

--- app/src/main/java/com/manbirkakkar/customtoast/MainActivity.kt
+++ app/src/main/java/com/manbirkakkar/customtoast/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.manbirkakkar.customtoast
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.manbirkakkar.customtoast.databinding.ActivityMainBinding
+import com.manbirkakkar.customtoastlibrary.ToastMessage
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var binding: ActivityMainBinding? = null
+    private val customMessage: String = "Test Message"
+    private val toastDuration: Int = 2000 // toast message duration in milliseconds
+    private val toastBackgroundColor: Int = Color.RED
+    private val toastTextColor: Int = Color.BLACK
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        val view: View = binding!!.root
+        setContentView(view)
+
+        binding?.showToast?.setOnClickListener(this)
+        binding?.showToastCustomDuration?.setOnClickListener(this)
+        binding?.colorChangeToast?.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View?) {
+        when(view){
+            binding?.showToast -> ToastMessage(this).showToast(customMessage)
+            binding?.showToastCustomDuration -> ToastMessage(this).showToast(customMessage, toastDuration)
+        }
     }
 }
